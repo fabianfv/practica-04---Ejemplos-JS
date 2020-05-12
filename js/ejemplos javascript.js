f5a3afde7779385048473b0ee8de061c88babaaa
fabianfv <fabianfloresvadell@gmail.com>
2020-05-12 19:27:01 +0200

    añadida la funcion auxiliar 'salida' que muestra el par (valor_esperado, valor_devuelto)

--- js/ejemplos javascript.js
+++ js/ejemplos javascript.js
@@ -1,2 +1,7 @@
+/*
+  La función auxiliar salida muestra el valor esperado y valor real de retorno de la función
+*/
 
-
+function salida(valor_esperado, valor_devuelto) {
+  console.log(`valor esperado: ${valor_esperado}\nvalor devuelto: ${valor_devuelto}`)
+}
