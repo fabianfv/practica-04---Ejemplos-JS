616074b5d781462b22e822b88142102d2e955311
fabianfv <fabianfloresvadell@gmail.com>
2020-05-12 19:30:37 +0200

    añadida la funcion siguiente_entero(n)

--- js/ejemplos javascript.js
+++ js/ejemplos javascript.js
@@ -28,3 +28,15 @@ function minutos_a_segudos(minutos) {
 salida(300, minutos_a_segundos(5))
 salida(180, minutos_a_segundos(3))
 salida(120, minutos_a_segundos(2))
+
+/*
+  Devolver el siguiente entero
+*/
+
+function siguiente_entero(n) {
+  return n > 0 ? ++n : --n
+}
+
+salida(1, siguiente_entero(0))
+salida(10, siguiente_entero(9))
+salida(-3, siguiente_entero(-2))
